23f0a6b85e104cc12ee5107fa0ceb803aa265225
jhon <jhlirion@gmail.com>
2022-09-20 19:59:49 -0500

    requim

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-import streamlit.components.v1 as html
+#from streamlit_option_menu import option_menu
+#import streamlit.components.v1 as html
 import pandas as pd
 import plotly.express as px
 import analisis
